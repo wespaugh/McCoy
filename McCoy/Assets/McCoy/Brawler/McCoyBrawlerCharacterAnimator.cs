fb192dd0a9a9fa83be6688d512025bcc0e4a004e
wespaugh <wespaugh@gmail.com>
2022-10-14 22:29:11 -0400

    Adding shoot sprites, animations, hitboxes and moves for rex. completely untested.

---
+++ McCoy/Assets/McCoy/Brawler/McCoyBrawlerCharacterAnimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.McCoy.Brawler
+{
+  public class McCoyBrawlerCharacterAnimator : MonoBehaviour
+  {
+    [SerializeField]
+    Animator gunAnimator = null;
+    public void Shoot()
+    {
+      gunAnimator.SetTrigger("Shoot");
+    }
+  }
+}
